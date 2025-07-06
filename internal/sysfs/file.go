24ce605633caf673e39f5eff6ca51dfc52780bb0
Christian Stewart <christian@aperture.us>
2025-07-06 01:48:36 -0700

    sysfs: use poll implementation for WithStdin
    If we pass os.Stdin to WithStdin, a special code path is used which implements
    Poll() with os-specific Poll APIs. This is used in poll_oneoff to implement
    asynchronous I/O with stdin as a special case:
    
    see wazero/internal/sys/fs.go
    
    - InitFSContext(stdin, stdout, ...)
      - inFile, err := stdinFileEntry(stdin)
        - if nil, no-op file descriptor
        - if *os.File, uses NewStdioFile(true, f)
        - otherwise returns &StdinFile{Reader: r}
    
    If we want to implement Poll with a custom io.Reader passed to WithStdin, this
    is currently not possible.
    
    This commit uses Poll on the io.Reader passed to WithStdin if present.
    
    See: https://github.com/tetratelabs/wazero/issues/1500#issuecomment-3041125375
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- internal/sysfs/file.go
+++ internal/sysfs/file.go
@@ -351,8 +351,17 @@ func (f *fsFile) SetNonblock(bool) experimentalsys.Errno {
 	return experimentalsys.ENOSYS
 }
 
+// pollable has just the Poll function.
+type pollable interface {
+	Poll(fsapi.Pflag, int32) (ready bool, errno experimentalsys.Errno)
+}
+
 // Poll implements the same method as documented on fsapi.File
-func (f *fsFile) Poll(fsapi.Pflag, int32) (ready bool, errno experimentalsys.Errno) {
+func (f *fsFile) Poll(flag fsapi.Pflag, timeoutMillis int32) (ready bool, errno experimentalsys.Errno) {
+	if p, isPollable := f.fs.(pollable); isPollable {
+		return p.Poll(flag, timeoutMillis)
+	}
+
 	return false, experimentalsys.ENOSYS
 }
 
