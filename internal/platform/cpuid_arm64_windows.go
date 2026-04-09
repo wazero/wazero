15f40827e4ed9d33d7cf80766166fad43dfd00bc
Nuno Cruces <ncruces@users.noreply.github.com>
2026-04-09 11:57:37 +0100

    Update golang.org/x/sys
    Signed-off-by: Nuno Cruces <ncruces@users.noreply.github.com>

---
+++ internal/platform/cpuid_arm64_windows.go
@@ -0,0 +1,17 @@
+//go:build !windows
+
+package platform
+
+import "golang.org/x/sys/windows"
+
+// CpuFeatures exposes the capabilities for this CPU, queried via the Has method.
+var CpuFeatures = loadCpuFeatureFlags()
+
+func loadCpuFeatureFlags() (flags CpuFeatureFlags) {
+	// See: go.dev/cl/757482
+	// golang.org/x/sys/cpu can't import golang.org/x/sys/windows and can't do this yet.
+	if windows.IsProcessorFeaturePresent(windows.PF_ARM_V81_ATOMIC_INSTRUCTIONS_AVAILABLE) {
+		flags |= CpuFeatureArm64Atomic
+	}
+	return
+}
